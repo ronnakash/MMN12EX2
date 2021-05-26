d4b9f3392248cf02f536ae310fc8caf5e6c64faf
ronna <ronnakash@gmail.com>
2021-05-27 01:43:51 +0300

    part B

--- src/Data.java
+++ src/Data.java
@@ -2,29 +2,40 @@ public class Data {
 
     private int x = 0;
     private int y = 0;
+    private int turn;
 
     public Data(int x, int y) {
         this.x = x;
         this.y = y;
+        turn = 1;
     }
 
     public synchronized int getDiff() {
-        try {
-            wait(50);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (turn==1) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        turn = 1;
+        notifyAll();
+
         return (Math.abs(x - y));
     }
 
     public synchronized void update(int dx, int dy) {
+        while (turn==0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         x = x + dx;
         y = y + dy;
-        try {
-            wait(50);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        turn = 0;
+        notifyAll();
     }
 
     public static void main(String[] args) {
